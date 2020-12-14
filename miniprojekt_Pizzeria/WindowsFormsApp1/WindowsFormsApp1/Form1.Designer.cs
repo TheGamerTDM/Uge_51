2af6291c37c443abfafe05a58644c35cd6dc888d
TheGamerTDM <61098144+TheGamerTDM@users.noreply.github.com>
2020-12-14 10:43:03 +0100

    for godkendt?

---
+++ miniprojekt_Pizzeria/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.katpizza49 = new System.Windows.Forms.Button();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.kamelpizza69 = new System.Windows.Forms.Button();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.klovpizza59 = new System.Windows.Forms.Button();
+            this.katpizzainfo = new System.Windows.Forms.Button();
+            this.kamelpizzainfo = new System.Windows.Forms.Button();
+            this.klovnpizzainfo = new System.Windows.Forms.Button();
+            this.buynow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(507, 25);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "WAX PIZZAz                             Ring til os: 99999996";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.Black;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox3.Enabled = false;
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox3.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox3.Location = new System.Drawing.Point(12, 92);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox3.Size = new System.Drawing.Size(86, 25);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "Kat pizza";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.Black;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Enabled = false;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox2.Location = new System.Drawing.Point(12, 128);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox2.Size = new System.Drawing.Size(116, 25);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "Kamel pizza";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.Black;
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.Enabled = false;
+            this.richTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox4.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox4.Location = new System.Drawing.Point(12, 163);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox4.Size = new System.Drawing.Size(110, 25);
+            this.richTextBox4.TabIndex = 5;
+            this.richTextBox4.Text = "Klovn pizza";
+            // 
+            // katpizza49
+            // 
+            this.katpizza49.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.katpizza49.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.katpizza49.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.katpizza49.ForeColor = System.Drawing.Color.White;
+            this.katpizza49.Location = new System.Drawing.Point(467, 80);
+            this.katpizza49.Name = "katpizza49";
+            this.katpizza49.Size = new System.Drawing.Size(40, 37);
+            this.katpizza49.TabIndex = 6;
+            this.katpizza49.Text = "+";
+            this.katpizza49.UseVisualStyleBackColor = false;
+            this.katpizza49.Click += new System.EventHandler(this.pris);
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.BackColor = System.Drawing.Color.Black;
+            this.richTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox5.Enabled = false;
+            this.richTextBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox5.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox5.Location = new System.Drawing.Point(386, 87);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.ReadOnly = true;
+            this.richTextBox5.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox5.Size = new System.Drawing.Size(75, 25);
+            this.richTextBox5.TabIndex = 7;
+            this.richTextBox5.Text = "DKK 49";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.BackColor = System.Drawing.Color.Black;
+            this.richTextBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox6.Enabled = false;
+            this.richTextBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox6.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox6.Location = new System.Drawing.Point(386, 128);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.ReadOnly = true;
+            this.richTextBox6.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox6.Size = new System.Drawing.Size(75, 25);
+            this.richTextBox6.TabIndex = 9;
+            this.richTextBox6.Text = "DKK 69";
+            // 
+            // kamelpizza69
+            // 
+            this.kamelpizza69.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.kamelpizza69.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.kamelpizza69.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kamelpizza69.ForeColor = System.Drawing.Color.White;
+            this.kamelpizza69.Location = new System.Drawing.Point(467, 121);
+            this.kamelpizza69.Name = "kamelpizza69";
+            this.kamelpizza69.Size = new System.Drawing.Size(40, 37);
+            this.kamelpizza69.TabIndex = 8;
+            this.kamelpizza69.Text = "+";
+            this.kamelpizza69.UseVisualStyleBackColor = false;
+            this.kamelpizza69.Click += new System.EventHandler(this.pris);
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.BackColor = System.Drawing.Color.Black;
+            this.richTextBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox7.Enabled = false;
+            this.richTextBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox7.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox7.Location = new System.Drawing.Point(386, 170);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.ReadOnly = true;
+            this.richTextBox7.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox7.Size = new System.Drawing.Size(75, 25);
+            this.richTextBox7.TabIndex = 11;
+            this.richTextBox7.Text = "DKK 59";
+            // 
+            // klovpizza59
+            // 
+            this.klovpizza59.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.klovpizza59.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.klovpizza59.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.klovpizza59.ForeColor = System.Drawing.Color.White;
+            this.klovpizza59.Location = new System.Drawing.Point(467, 163);
+            this.klovpizza59.Name = "klovpizza59";
+            this.klovpizza59.Size = new System.Drawing.Size(40, 37);
+            this.klovpizza59.TabIndex = 10;
+            this.klovpizza59.Text = "+";
+            this.klovpizza59.UseVisualStyleBackColor = false;
+            this.klovpizza59.Click += new System.EventHandler(this.pris);
+            // 
+            // katpizzainfo
+            // 
+            this.katpizzainfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.katpizzainfo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.katpizzainfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.katpizzainfo.ForeColor = System.Drawing.Color.White;
+            this.katpizzainfo.Location = new System.Drawing.Point(307, 80);
+            this.katpizzainfo.Name = "katpizzainfo";
+            this.katpizzainfo.Size = new System.Drawing.Size(73, 37);
+            this.katpizzainfo.TabIndex = 12;
+            this.katpizzainfo.Text = "INFO";
+            this.katpizzainfo.UseVisualStyleBackColor = false;
+            this.katpizzainfo.Click += new System.EventHandler(this.info);
+            // 
+            // kamelpizzainfo
+            // 
+            this.kamelpizzainfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.kamelpizzainfo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.kamelpizzainfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kamelpizzainfo.ForeColor = System.Drawing.Color.White;
+            this.kamelpizzainfo.Location = new System.Drawing.Point(307, 123);
+            this.kamelpizzainfo.Name = "kamelpizzainfo";
+            this.kamelpizzainfo.Size = new System.Drawing.Size(73, 37);
+            this.kamelpizzainfo.TabIndex = 13;
+            this.kamelpizzainfo.Text = "INFO";
+            this.kamelpizzainfo.UseVisualStyleBackColor = false;
+            this.kamelpizzainfo.Click += new System.EventHandler(this.info);
+            // 
+            // klovnpizzainfo
+            // 
+            this.klovnpizzainfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.klovnpizzainfo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.klovnpizzainfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.klovnpizzainfo.ForeColor = System.Drawing.Color.White;
+            this.klovnpizzainfo.Location = new System.Drawing.Point(307, 166);
+            this.klovnpizzainfo.Name = "klovnpizzainfo";
+            this.klovnpizzainfo.Size = new System.Drawing.Size(73, 37);
+            this.klovnpizzainfo.TabIndex = 14;
+            this.klovnpizzainfo.Text = "INFO";
+            this.klovnpizzainfo.UseVisualStyleBackColor = false;
+            this.klovnpizzainfo.Click += new System.EventHandler(this.info);
+            // 
+            // buynow
+            // 
+            this.buynow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buynow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buynow.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buynow.ForeColor = System.Drawing.Color.White;
+            this.buynow.Location = new System.Drawing.Point(201, 209);
+            this.buynow.Name = "buynow";
+            this.buynow.Size = new System.Drawing.Size(133, 65);
+            this.buynow.TabIndex = 16;
+            this.buynow.Text = "BUY  NOW";
+            this.buynow.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(530, 292);
+            this.Controls.Add(this.buynow);
+            this.Controls.Add(this.klovnpizzainfo);
+            this.Controls.Add(this.kamelpizzainfo);
+            this.Controls.Add(this.katpizzainfo);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.klovpizza59);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.kamelpizza69);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.katpizza49);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MenuKort";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button katpizza49;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.Button kamelpizza69;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.Button klovpizza59;
+        private System.Windows.Forms.Button katpizzainfo;
+        private System.Windows.Forms.Button kamelpizzainfo;
+        private System.Windows.Forms.Button klovnpizzainfo;
+        private System.Windows.Forms.Button buynow;
+    }
+}
+
